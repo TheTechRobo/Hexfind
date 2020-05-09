9134812b7b6ef58e4d669a42a7af8e7938ff8f57
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2020-05-09 16:45:00 -0400

    Features ++ Bugfixes ++ Null\n right after seeing olivier's mum at the door, she is still here but...I wanted to Commit.

--- HexFinder.py
+++ HexFinder.py
@@ -1,6 +1,11 @@
-from tkinter import colorchooser
+from tkinter import colorchooser, Button, Label
 import tkinter
 window = tkinter.Tk()
-hello = colorchooser.askcolor()
-print(hello)
+window.title("Hexfind")
+def hi():
+    hello = colorchooser.askcolor()
+    print(hello)
+    Label(window, text="RESULT:").pack()
+    Label(window, text=hello).pack()
+Button(window, text="Ask for Colour", command=hi).pack()
 window.mainloop()
